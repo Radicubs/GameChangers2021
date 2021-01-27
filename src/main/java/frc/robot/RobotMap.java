f4f06e5287fbde3e4cb6675e6ef3a45cc9577cda
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-01-26 18:46:54 -0600

    basic setup

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,48 @@
+package frc.robot;
+
+public class RobotMap {
+
+  // JOYSTICKS
+  public static final int JOYSTICK = 0;
+
+  // BUTTON BOARD
+  public static final int BUTTON_BOARD = 1;
+
+  // JOYSTICK AXES
+  public static final int LEFT_X_AXIS = 0;
+  public static final int LEFT_Y_AXIS = 1;
+  public static final int LT_AXIS = 2;
+  public static final int RT_AXIS = 3;
+  public static final int RIGHT_X_AXIS = 4;
+  public static final int RIGHT_Y_AXIS = 5;
+
+  // BUTTONS ON CONTROLLER
+  public static final int A_BUTTON = 1;
+  public static final int B_BUTTON = 2;
+  public static final int X_BUTTON = 3;
+  public static final int Y_BUTTON = 4;
+  public static final int LB_BUTTON = 5;
+  public static final int RB_BUTTON = 6;
+  public static final int GO_BACK_BUTTON = 7;
+  public static final int START_BUTTON = 8;
+  public static final int CLICK_LEFT_JOYSTICK = 9;
+  public static final int CLICK_RIGHT_JOYSTICK = 10;
+  
+  // BUTTON BOARD BUTTONS: 1 --> 4 GOES LEFT TO RIGHT
+  public static final int TOP_BUTTON_ONE = 4;
+  public static final int TOP_BUTTON_TWO = 3;
+  public static final int TOP_BUTTON_THREE = 2;
+  public static final int TOP_BUTTON_FOUR = 1;
+  public static final int BOTTOM_BUTTON_ONE = 8;
+  public static final int BOTTOM_BUTTON_TWO = 7;
+  public static final int BOTTOM_BUTTON_THREE = 6;
+  public static final int BOTTOM_BUTTON_FOUR = 5;
+
+  // PWM MOTORS
+  public static final int LEFT_TALON = 6;
+  public static final int LEFT_FOLLOWER_ONE = 4;
+
+  public static final int RIGHT_TALON = 7;
+  public static final int RIGHT_FOLLOWER_ONE = 5;
+
+}
