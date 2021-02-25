ad8c38273427e9ab02cc47b5c1df86ccbd14e7c0
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-02-24 18:39:16 -0600

    finished autonav A

--- src/main/java/frc/robot/commands/auto/AutoNavA.java
+++ src/main/java/frc/robot/commands/auto/AutoNavA.java
@@ -7,12 +7,20 @@ import frc.robot.RobotMap;
 
 public class AutoNavA extends CommandGroup {
     private final double speed = 0.25;
+    private final int DISTANCE_MULTIPLIER = 1;
 
     public AutoNavA() {
         requires(Robot.driveTrain);
-        addSequential(new Drive(Math.PI / 2, speed, 0), 1);
-        addSequential(new Drive(0, speed, 0), 1);
-        addSequential(new Drive(Math.PI / 2, -speed, 0), 1);
-        addSequential(new Drive(0, -speed, 0), 1);
+        addSequential(new Drive(Math.PI / 2, speed, 0), 3 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(Math.PI, speed, 0), 1 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(3 * Math.PI / 2, speed, 0), 1 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(0, speed, 0), 1 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(Math.PI / 6, speed, 0), 4 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(0, speed, 0), 0.5 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(3 * Math.PI / 2, speed, 0), 1 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(5 * Math.PI / 6, speed, 0), 2.5 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(Math.PI / 2, speed, 0), 1 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(0, speed, 0), 1.3 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(3 * Math.PI / 2, speed, 0), 1.3 * DISTANCE_MULTIPLIER);
     }
 }
