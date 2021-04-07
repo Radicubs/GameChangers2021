95c40a36e5c275a61cf7b0a2c8561ed90d0367a9
AnanthVivekanand <avivek3021@gmail.com>
2021-04-06 20:10:51 -0500

    Shooter constant velocity with PID

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -11,7 +12,8 @@ public class Shooter extends Subsystem {
   private TalonFX shooterMotorOne;
   private TalonFX shooterMotorTwo;
 
-  private double speed = 0.55;
+  private double speed = 11500;
+  // private double speed = 0.1;
 
   public Shooter() {
 
@@ -24,11 +26,25 @@ public class Shooter extends Subsystem {
 
     shooterMotorOne.setNeutralMode(NeutralMode.Coast);
     shooterMotorTwo.setNeutralMode(NeutralMode.Coast);
+
+    shooterMotorOne.setInverted(true);
+    shooterMotorOne.setSensorPhase(false);
+    shooterMotorTwo.follow(shooterMotorOne);
+    shooterMotorOne.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
+
+    shooterMotorOne.config_kP(0, .05);
+    shooterMotorOne.config_kF(0, 0.0499999523);
+    shooterMotorOne.config_kD(0, .9);
   }
 
   public void shootBall(double speed) {
-    shooterMotorOne.set(ControlMode.PercentOutput, -speed);
-    shooterMotorTwo.set(ControlMode.PercentOutput, speed);
+    // shooterMotorOne.set(ControlMode.PercentOutput, speed);
+    // shooterMotorOne.set(ControlMode.Velocity, -11500);
+    shooterMotorOne.set(ControlMode.Velocity, speed);
+    System.out.println("meow");
+    System.out.println(shooterMotorOne.getSelectedSensorVelocity());
+    System.out.println(shooterMotorOne.getMotorOutputPercent());
+    // shooterMotorTwo.set(ControlMode.PercentOutput, speed);
   }
 
   @Override
