f38ca5d1fbe3f5067d02e3d50e5ec28861b4b254
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2021-04-08 06:39:02 -0500

    Added AutoNav A

--- src/main/java/frc/robot/commands/auto/AutoNavAHard.java
+++ src/main/java/frc/robot/commands/auto/AutoNavAHard.java
@@ -12,32 +12,39 @@ public class AutoNavAHard extends CommandGroup {
     public AutoNavAHard() {
         requires(Robot.driveTrain);
         // forward
-        addSequential(new Drive(0, speed*2, 0), 2 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(0, speed * 2, 0), 2.15 * DISTANCE_MULTIPLIER);
         // right
-        addSequential(new Drive(-Math.PI / 2, speed*2, 0), 1.35 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(-Math.PI / 2, speed * 2, 0), 1.35 * DISTANCE_MULTIPLIER);
         // back
-        addSequential(new Drive(0, -speed*2, 0), 0.85 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(0, -speed * 2, 0), 0.90 * DISTANCE_MULTIPLIER);
         // left
-        addSequential(new Drive(Math.PI / 2, speed*2.4, 0), 1.30 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(Math.PI / 2, speed * 2.4, 0), 1.30 * DISTANCE_MULTIPLIER);
         // forward to second
-        addSequential(new Drive(0, speed*3, 0), 2 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(0, speed * 3, 0), 2 * DISTANCE_MULTIPLIER);
         // left
-        addSequential(new Drive(Math.PI / 2, speed*2, 0), 1.35 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(Math.PI / 2, speed * 2, 0), 1.35 * DISTANCE_MULTIPLIER);
         // back
-        addSequential(new Drive(0, -speed*2, 0), 0.85 * DISTANCE_MULTIPLIER);
+        addSequential(new Drive(0, -speed * 2, 0), 1.15 * DISTANCE_MULTIPLIER);
         // right
-        addSequential(new Drive(-Math.PI / 2, speed*2, 0), 1.35 * DISTANCE_MULTIPLIER);
-
-
+        addSequential(new Drive(-Math.PI / 2, speed * 2, 0), 1.45 * DISTANCE_MULTIPLIER);
+        // Diagnol
+        addSequential(new Drive(-Math.PI / 2.75, speed * 2, 0), 1.55 * DISTANCE_MULTIPLIER);
+        // Straight
+        addSequential(new Drive(0, speed * 2, 0), 1.50 * DISTANCE_MULTIPLIER);
+        // left
+        addSequential(new Drive(Math.PI / 2, speed * 2, 0), 1.6 * DISTANCE_MULTIPLIER);
+        // back
+        addSequential(new Drive(0, -speed * 4, 0), 4 * DISTANCE_MULTIPLIER);
 
         /*
-        addSequential(new Drive(-5 * Math.PI / 6, speed, 0), 4 * DISTANCE_MULTIPLIER);
-        addSequential(new Drive(3 * Math.PI / 2, speed, 0), 0.5 * DISTANCE_MULTIPLIER);
-        addSequential(new Drive(Math.PI / 2, speed, 0), 1 * DISTANCE_MULTIPLIER);
-        addSequential(new Drive(1 * Math.PI / 3, speed, 0), 2.5 * DISTANCE_MULTIPLIER);
-        addSequential(new Drive(0, speed, 0), 1 * DISTANCE_MULTIPLIER);
-        addSequential(new Drive(3 * Math.PI / 2, speed, 0), 1.3 * DISTANCE_MULTIPLIER);
-        addSequential(new Drive(Math.PI, speed, 0), 1.3 * DISTANCE_MULTIPLIER);
-    */
+         * addSequential(new Drive(-5 * Math.PI / 6, speed, 0), 4 *
+         * DISTANCE_MULTIPLIER); addSequential(new Drive(3 * Math.PI / 2, speed, 0), 0.5
+         * * DISTANCE_MULTIPLIER); addSequential(new Drive(Math.PI / 2, speed, 0), 1 *
+         * DISTANCE_MULTIPLIER); addSequential(new Drive(1 * Math.PI / 3, speed, 0), 2.5
+         * * DISTANCE_MULTIPLIER); addSequential(new Drive(0, speed, 0), 1 *
+         * DISTANCE_MULTIPLIER); addSequential(new Drive(3 * Math.PI / 2, speed, 0), 1.3
+         * * DISTANCE_MULTIPLIER); addSequential(new Drive(Math.PI, speed, 0), 1.3 *
+         * DISTANCE_MULTIPLIER);
+         */
     }
 }
